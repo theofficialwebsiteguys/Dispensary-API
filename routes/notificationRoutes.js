aaf70a7e4ea84acf47936631b77cc2fceb7d5c42
Full Name <theofficialwebsiteguys@gmail.com>
2025-01-31 16:25:53 -0500

    update

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -4,7 +4,6 @@ const notificationController = require('../controllers/notificationController');
 const router = express.Router();
 
 router.get('/all', notificationController.getUserNotifications);
-
 router.post('/send-push', notificationController.sendPush);
 
 router.delete('/delete/:notificationId', notificationController.deleteNotification);
